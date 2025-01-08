8bcc1e5edf545e05b6844e65f06048137ffe8757
Nicolas09-COL <nicosanfo@hotmail.com>
2025-01-08 15:14:04 -0500

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,26 @@
+const display = document.getElementById('display');
+let currentInput = '';
+
+function appendValue(value) {
+    currentInput += value;
+    display.value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    display.value = currentInput;
+}
+
+function calculate() {
+    try {
+        currentInput = eval(currentInput);
+        display.value = currentInput;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
